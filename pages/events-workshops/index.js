2dac33ed31ffc73bbc5044eb60ba07b3c992c6d2
Ajr khan <git config --global user.email you@example.com>
2023-01-12 14:44:45 +0530

    ddsd

--- pages/events-workshops/index.js
+++ pages/events-workshops/index.js
@@ -70,6 +70,32 @@ export default function EventsAndWorkshop() {
                      </div>
                   </div>
 
+                  <div className="col-sm-4">
+                     <div className="choose-card">
+                        <img src="/assets/img/workshopformain1.jpg" alt="Images"/>
+                        <h3>Conversational Capacity - Virtual</h3>
+                        <p>February 10th, 2023</p>
+                        <div className='row eventsbottomarea2 text-center'>
+                           <div className='col-sm-12'>
+                              <a href="/events-workshops/virtual-public-workshop#REQUEST" className="default-btn-two">REQUEST INFO<i className="bx bx-right-arrow-alt"></i></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div className="col-sm-4">
+                     <div className="choose-card">
+                        <img src="/assets/img/workshopformain1.jpg" alt="Images"/>
+                        <h3>Building Trust - Virtual</h3>
+                        <p>February 22nd, 2023</p>
+                        <div className='row eventsbottomarea2 text-center'>
+                           <div className='col-sm-12'>
+                              <a href="/events-workshops/virtual-public-workshop#REQUEST" className="default-btn-two">REQUEST INFO<i className="bx bx-right-arrow-alt"></i></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+
                   
                  
                </div>
--- pages/events-workshops/virtual-public-workshop.js
+++ pages/events-workshops/virtual-public-workshop.js
@@ -111,6 +111,8 @@ export default function VirtualPublicWorkshop() {
                                        <option value="The SLII Experience™ - Virtual - January 23rd - 24th, 2023">The SLII Experience™ - Virtual - January 23rd - 24th, 2023</option>
                                        <option value="The SLII Experience™ T4T - January 30th - 31st, 2023">The SLII Experience™ T4T - January 30th - 31st, 2023</option>
                                        <option value="Self Leadership - Virtual - January 31st, 2023">Self Leadership - Virtual - January 31st, 2023</option>
+                                       <option value="Conversational Capacity - Virtual - February 10th, 2023">Conversational Capacity - Virtual - February 10th, 2023</option>
+                                       <option value="Building Trust - Virtual - February 22nd, 2023">Building Trust - Virtual - February 22nd, 2023</option>
                                     </select>
                                  </div>
                               </div>
@@ -184,6 +186,32 @@ export default function VirtualPublicWorkshop() {
                                  </div>
                               </div>
                            </div>
+
+                           <div className="col-sm-4">
+                     <div className="choose-card">
+                        <img src="/assets/img/workshopformain1.jpg" alt="Images"/>
+                        <h3>Conversational Capacity - Virtual</h3>
+                        <p>February 10th, 2023</p>
+                        <div className='row eventsbottomarea2 text-center'>
+                           <div className='col-sm-12'>
+                              <a href="/events-workshops/virtual-public-workshop#REQUEST" className="default-btn-two">REQUEST INFO<i className="bx bx-right-arrow-alt"></i></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div className="col-sm-4">
+                     <div className="choose-card">
+                        <img src="/assets/img/workshopformain1.jpg" alt="Images"/>
+                        <h3>Building Trust - Virtual</h3>
+                        <p>February 22nd, 2023</p>
+                        <div className='row eventsbottomarea2 text-center'>
+                           <div className='col-sm-12'>
+                              <a href="/events-workshops/virtual-public-workshop#REQUEST" className="default-btn-two">REQUEST INFO<i className="bx bx-right-arrow-alt"></i></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
 
 
                         </div>
