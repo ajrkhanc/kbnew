2bce1f99665d2b9bea9d2ff2fd7dd07e837b3f28
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-02-07 15:13:28 +0530

    wrerw

--- pages/podcast/beating-burnout-with-christina-maslach.js
+++ pages/podcast/beating-burnout-with-christina-maslach.js
@@ -6,7 +6,7 @@ export default function PodCastinner() {
         <>
         <Head>
         <title>Podcast - Beating Burnout with Christina Maslach</title>
-        <meta name="description" content="Beating Burnout with Christina Maslach"/>
+        <meta name="description" content="Hear Christina Maslach talk about how to recognize the signs of employee burnout and how to combat the issue to promote increased productivity and health."/>
         <link rel="icon" href="/favicon.ico" />
         </Head>
          
