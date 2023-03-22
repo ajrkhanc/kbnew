f853e5a14a18b8d5850121cf535e6cd0f4bba22f
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-03-22 16:54:06 +0530

    wwwW

--- components/Assesmment/Footer.js
+++ components/Assesmment/Footer.js
@@ -31,23 +31,14 @@ function Footer() {
   }
 
     return (
-        <footer className="footer-area footer-bg2">
-         <div className="footer-top pt-100 pb-70">
+        <footer className="footer-area footer-bg2 massesment pt-20">
+         <div className="footer-top pb-10">
             <div className="container">
                <div className="row">
-                  <div className="col-lg-3 col-md-6">
-                     <div className="footer-widget footer-widget-color1">
-                        <div className="footer-logo">
-                           <Link href="/"><a>
-                           <img src="/assets/img/whitelogo.png" alt="Images"/>
-                           </a>
-                           </Link>
-                        </div>
-                        <p>
-                           Blanchard Research and Training LLP is the leading training provider in South Asia, with expertise in leadership development across hierarchies.
-                        </p>
+                  <div className="col-sm-3">
+                     <div className="footer-widget footer-widget-color1">                       
                         <div className="social-link-content">
-                           <h3 className="title">Follow Us On</h3>
+                           {/* <h3 className="title">Follow Us On</h3> */}
                            <ul className="social-footer-link">
                               <li>
                                  <a href="https://www.facebook.com/blanchardresearchandtrainingindia" target="_blank">
@@ -78,126 +69,22 @@ function Footer() {
                         </div>
                      </div>
                   </div>
-                  <div className="col-lg-2 col-md-6">
-                     <div className="footer-widget footer-widget-color1 ps-2">
-                        <h3>CONNECT</h3>
-                        <ul className="footer-list">
-                           <li>
-                              <Link href="#" target="_blank">
-                              Contact Us
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Request a Speaker
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Terms And Conditions
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Cancellation and Refund Policy
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Books
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Privacy Policy
-                              </Link>
-                           </li>
-                        </ul>
-                     </div>
-                  </div>
-                  <div className="col-lg-3 col-md-6">
-                     <div className="footer-widget footer-widget-color1 ps-2">
-                        <h3>PRODUCTS & SERVICES</h3>
-                        <ul className="footer-list">
-                           <li>
-                              <Link href="#" target="_blank">
-                              The SLII Experience™
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Blanchard Management Essentials
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Self Leadership
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Coaching Essentials
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Building Trust
-                              </Link>
-                           </li>
-                           <li>
-                              <Link href="#" target="_blank">
-                              Leading People Through Change
-                              </Link>
-                           </li>
-						   <li>
-                              <Link href="#" target="_blank">
-                              Team Leadership
-                              </Link>
-                           </li>
-                        </ul>
-                     </div>
-                  </div>
-                  <div className="col-lg-4 col-md-6">
+                 
+                  <div className="col-sm-9">
                      <div className="footer-widget footer-widget-color1">
-                        <h3>GET IN TOUCH</h3>
+                        {/* <h3>GET IN TOUCH</h3> */}
                         <ul className="footer-list-two">
                            <li>
                               <i className="flaticon-placeholder"></i>
-                              <div className="title">BYLD Group: <br></br>2nd and 3rd Floor, Plot No. 48, Sector - 44, Opp. EPF Regional Office Gurugram, Haryana - 122003</div>
-                           </li>
-                           <li>
-                              <i className="flaticon-call"></i>
-                              <div className="title">Phone:</div>
-                              <a href="tel:1800-102-1345"> 1800-102-1345</a>
-                           </li>
-                           <li>
-                              <i className="flaticon-email"></i>
-                              <div className="title">Mail:</div>
-                              <a href="mailto:blanchard.info@byldgroup.com"><span className="__cf_email__" data-cfemail="ff979a939390bf91968790d19c9092">blanchard.info@byldgroup.com</span></a>
-                           </li>
-                        </ul>
-                        <form id="footerformreset" className="newsletter-form" onSubmit={registerUser}>
-                           <input type="email" className="form-control" placeholder="Email Address" name="fmail" required autocomplete="off"/>
-                           <button type="submit" className="submit-btn btn-bg1">
-                           Subscribe
-                           </button>
-                           <div id="validator-newsletter" className="form-result"></div>
-                        </form>
-                        <p id="showlabel" style={{ display: "none" }}></p>
+                              <div className="title">BYLD Group: 2nd and 3rd Floor, Plot No. 48, Sector - 44, Opp. EPF Regional Office Gurugram, Haryana - 122003</div>
+                           </li>                           
+                        </ul>                  
                      </div>
                   </div>
                </div>
             </div>
          </div>
-         <div className="container">
-            <div className="copy-right-area-two">
-               <div className="copy-right-text">
-                  <p>
-                    © 2022 Blanchardinternational India . All Rights Reserved.
-                  </p>
-               </div>
-            </div>
-         </div>
+   
       </footer>
     )
 }
--- pages/assessments/bme-assessment/index.js
+++ pages/assessments/bme-assessment/index.js
@@ -60,7 +60,7 @@ export default function EdgeManagerAssessment() {
                xhttp.onload = function () {
                   console.log(this.responseText);
                }
-               xhttp.open("Post", 'https://ajrkhan.xyz/byldgroup/wp-json/contact-form-7/v1/contact-forms/53/feedback');
+               xhttp.open("Post", 'https://ajrkhan.xyz/blanchardinternational/wp-json/contact-form-7/v1/contact-forms/319/feedback');
                xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 
