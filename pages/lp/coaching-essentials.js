950df9ee7add6c536bd766cc2e5eced59da5964b
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-04-04 10:54:21 +0530

    qwety

--- pages/lp/coaching-essentials.js
+++ pages/lp/coaching-essentials.js
@@ -100,7 +100,7 @@ export default function HybridWorkplaces() {
                            <div className="form-group">
                               <label>Program Name*</label>                              
                               <select className="form-control" name="interest_in" required>
-                                 <option value="The Manager Who Can Coach: 26th August, 2022">The Manager Who Can Coach: 26th August, 2022</option>
+                                 <option value="The Manager Who Can Coach: 26th May, 2023">The Manager Who Can Coach: 26th May, 202</option>
                               </select>
                               <div className="help-block with-errors"></div>
                            </div>
@@ -164,7 +164,7 @@ export default function HybridWorkplaces() {
                </div>
                <div className="col-lg-6">
                   <div className="">
-                     <img src="/assets/img/coaching-webinar.jpg" alt="Images"/>
+                     <img src="/assets/img/coaching-wrk.jpg" alt="Images"/>
                   </div>
                </div>
             </div>
