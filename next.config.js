2ae35184d8479507744ecbcbc03f462f3b1f947c
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-08-07 14:51:08 +0530

    daqwe
    wqewq

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   redirects: async () => [
     {
-      source: '/:path',
+      source: '/',
       destination: 'https://byldgroup.com/blanchardindia',
       permanent: true
     },
