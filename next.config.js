61d6a12abab9111e36a2122d4e495883cef7cd5a
ajrkhan1 <102628339+ajrkhanc@users.noreply.github.com>
2023-08-07 14:11:01 +0530

    dd
    dsd

--- next.config.js
+++ next.config.js
@@ -7,16 +7,6 @@ module.exports = {
       destination: 'https://blanchardinternational.co.in/:path*',
       permanent: true
     },
-    {
-      source: '/events-workshops/program-experience-webinar1/',
-      destination: '/events-workshops',
-      permanent: true,
-    },
-    {
-      source: '/assessments/bme-assessment',
-      destination: '/assessments/bme-assessment-m',
-      permanent: true,
-    },
 
     {
       source: '/blog',
@@ -59,6 +49,188 @@ module.exports = {
       destination: 'https://byldgroup.com/blanchardindia/lp/:path*',
       permanent: false
     },
+
+    {
+      source: '/events-workshops/program-experience-webinar1/',
+      destination: 'https://byldgroup.com/blanchardindia/events-workshops',
+      permanent: true,
+    },
+
+    {
+      source: '/events-workshops',
+      destination: 'https://byldgroup.com/blanchardindia/events-workshops',
+      permanent: true,
+    },
+
+    {
+      source: '/events-workshops/virtual-public-workshop',
+      destination: 'https://byldgroup.com/blanchardindia/events-workshops/virtual-public-workshop',
+      permanent: true,
+    },
+
+    {
+      source: '/assessments/bme-assessment-m',
+      destination: 'https://byldgroup.com/blanchardindia/assessments/bme-assessment-m',
+      permanent: true,
+    },
+
+    {
+      source: '/about-us',
+      destination: 'https://byldgroup.com/blanchardindia/about-us',
+      permanent: true,
+    },
+
+    {
+      source: '/about-us/meet-the-team',
+      destination: 'https://byldgroup.com/blanchardindia/about-us/who-we-are/leadership-team',
+      permanent: true,
+    },
+
+    {
+      source: '/about-us/meet-the-team/yogesh-sood',
+      destination: 'https://byldgroup.com/blanchardindia/about-us/who-we-are/leadership-team/yogesh-sood',
+      permanent: true,
+    },
+
+    {
+      source: '/books',
+      destination: 'https://byldgroup.com/blanchardindia/books',
+      permanent: true,
+    },
+
+    {
+      source: '/elearning',
+      destination: 'https://byldgroup.com/blanchardindia/elearning',
+      permanent: true,
+    },
+
+    {
+      source: '/blanchard-privacy-policy',
+      destination: 'https://byldgroup.com/blanchardindia/blanchard-privacy-policy',
+      permanent: true,
+    },
+
+    {
+      source: '/cancellation-and-refund-policy',
+      destination: 'https://byldgroup.com/blanchardindia/cancellation-and-refund-policy',
+      permanent: true,
+    },
+
+    {
+      source: '/coaching-essentials-webinar',
+      destination: 'https://byldgroup.com/blanchardindia/coaching-essentials-webinar',
+      permanent: true,
+    },
+
+    {
+      source: '/get-started',
+      destination: 'https://byldgroup.com/blanchardindia/get-started',
+      permanent: true,
+    },
+
+    {
+      source: '/terms-and-conditions',
+      destination: 'https://byldgroup.com/blanchardindia/terms-and-conditions',
+      permanent: true,
+    },
+
+    {
+      source: '/thank-you-lp',
+      destination: 'https://byldgroup.com/blanchardindia/thank-you-lp',
+      permanent: true,
+    },
+
+    {
+      source: '/thank-you-lp-blanchard-generic-landing-page',
+      destination: 'https://byldgroup.com/blanchardindia/thank-you-lp-blanchard-generic-landing-page',
+      permanent: true,
+    },
+
+    {
+      source: '/the-three-secrets-of-the-new-one-minute-manager',
+      destination: 'https://byldgroup.com/blanchardindia/the-three-secrets-of-the-new-one-minute-manager',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/slii-experience',
+      destination: 'https://byldgroup.com/blanchardindia/what-we-do/leadership-development/leadership-development-programs',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/blanchard-management-essentials',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/blanchard-management-essentials',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/self-leadership',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/self-leadership',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/coaching-essentials',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/coaching-essentials',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/building-trust',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/building-trust',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/leading-people-through-change',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/leading-people-through-change',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/team-leadership',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/team-leadership',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/customer-service-training',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/legendary-service',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/online-learning-training',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/leading-virtually',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/online-learning-training',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/leading-virtually',
+      permanent: true,
+    },
+
+    {
+      source: '/products-services/conversational-capacity',
+      destination: 'https://byldgroup.com/blanchardindia/our-content/programs/conversational-capacity',
+      permanent: true,
+    },
+
+    
+
+    // {
+    //   source: '/aboutkkk',
+    //   destination: 'https://byldgroup.com/blanchardindia/abouthhhh',
+    //   permanent: true,
+    // },
+
+    // {
+    //   source: '/aboutkkk',
+    //   destination: 'https://byldgroup.com/blanchardindia/abouthhhh',
+    //   permanent: true,
+    // },
 
   ]
 }
